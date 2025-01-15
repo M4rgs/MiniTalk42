fe26996ecab397fb37a6fe57913bc098173430d3
Taha Mounir <tamounir@e1r6p4.1337.ma>
2025-01-15 08:00:06 +0100

    minitalk42

--- minitalk.h
+++ minitalk.h
@@ -6,19 +6,16 @@
 /*   By: tamounir <tamounir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 08:23:34 by tamounir          #+#    #+#             */
-/*   Updated: 2025/01/13 08:23:35 by tamounir         ###   ########.fr       */
+/*   Updated: 2025/01/15 07:58:04 by tamounir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <ctype.h>
 # include <signal.h>
-# include <sys/types.h>
 
 int		ft_atoi(char *str);
 void	ft_putchar_fd(char c, int fd);
